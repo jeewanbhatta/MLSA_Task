b6d42d1d0c9ced0ee700ffdc4a0932a1f65f8324
Jeewan Bhatta <86247623+jeewanbhatta@users.noreply.github.com>
2023-10-08 15:08:49 +0545

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,24 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/design.jpeg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "purple and blue abstract design background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a webapp Pentester.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Webapp pentesting",
+  "Network Pentesting",
+  "Vulnerability Assessment",
 ];
 
 /**
